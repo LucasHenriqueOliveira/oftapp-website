5cad0a49b84042f49ffb4c251b8641d4fa9fe692
Lucas <lucashen@gmail.com>
2015-06-22 17:31:05 -0300

    initial page with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oftappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the oftappApp
+ */
+angular.module('oftappApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oftappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the oftappApp
+ */
+angular.module('oftappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
