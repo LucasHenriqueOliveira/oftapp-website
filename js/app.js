e320810fda456b729056009da872d4847698aa7c
Lucas <lucashen@gmail.com>
2015-06-27 14:28:17 -0300

    add validate in front end with angularjs

--- js/app.js
+++ js/app.js
@@ -9,9 +9,45 @@
 
     function OftappController($scope) {
         $scope.submit = submit;
+        $scope.error_email = false;
+        $scope.error_name = false;
+        $scope.error_message = false;
+
+        $scope.$watch('email', function (val){
+            if($scope.email){
+                $scope.error_email = false;
+            }
+        });
+
+        $scope.$watch('name', function (val){
+            if($scope.name){
+                $scope.error_name = false;
+            }
+        });
+
+        $scope.$watch('message', function (val){
+            if($scope.message){
+                $scope.error_message = false;
+            }
+        });
 
         function submit(){
-            console.log($scope.email);
+            if(!$scope.email){
+                $scope.error_email = true;
+            }
+
+            if(!$scope.name){
+                $scope.error_name = true;
+            }
+
+            if(!$scope.message){
+                $scope.error_message = true;
+            }
+
+            if(!$scope.email || !$scope.name || !$scope.message){
+                return false;
+            }
+
         }
 
     }
